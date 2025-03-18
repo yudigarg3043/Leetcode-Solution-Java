cf2b7080adfc7559184d8f917980e661b54a43b1
yudigarg3043 <yudishgarg645@gmail.com>
2025-03-18 13:09:42 +0530

    Reverse Integer

---
+++ Medium/Q7. Reverse Integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while(x!=0){
+            rev = (rev*10) + (x%10);
+            x/=10;
+        }
+        if(rev>=Integer.MIN_VALUE && rev<=Integer.MAX_VALUE) return (int)rev;
+        else return 0;
+    }
+}
